4adab6afacb1a119622a4adb3760b0d195b846c7
Ryan Feigenbaum <director@philsci.org>
2021-02-17 17:40:10 -0500

    updated sort to account for lowercase

--- _data/eleventyComputed.js
+++ _data/eleventyComputed.js
@@ -1,11 +1,11 @@
 module.exports = {
   eleventyComputed: {
-    memberBooksSorted: (data) => {
+    memberBooksSorted: data => {
       const sortedTitles = data.member.sort((a, b) => {
-        if (a.last_name < b.last_name) {
+        if (a.last_name.toLowerCase() < b.last_name.toLowerCase()) {
           return -1;
         }
-        if (a.last_name > b.last_name) {
+        if (a.last_name.toLowerCase() > b.last_name.toLowerCase()) {
           return 1;
         }
         return 0;
