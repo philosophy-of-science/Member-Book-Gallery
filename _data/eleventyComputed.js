8838443bd9604a5210bc435e5287b5a1083b43c3
philosophy-of-science <director@philsci.org>
2021-02-24 11:18:33 -0500

    rm console.log

--- _data/eleventyComputed.js
+++ _data/eleventyComputed.js
@@ -1,7 +1,6 @@
 module.exports = {
   eleventyComputed: {
     memberData: data => {
-      console.log(data);
       const shuffle = arr => {
         for (let i = arr.length - 1; i > 0; i--) {
           const j = Math.floor(Math.random() * (i + 1));
